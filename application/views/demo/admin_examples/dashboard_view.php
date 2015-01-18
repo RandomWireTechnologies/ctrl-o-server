0f587f390d0bc04a516ba7d5ed085c8d6a1159f7
Patrick Shields <patrick@randomwire.biz>
2015-01-18 15:56:55 -0500

    updating admin dashboard

--- application/views/demo/admin_examples/dashboard_view.php
+++ application/views/demo/admin_examples/dashboard_view.php
@@ -42,13 +42,22 @@
 				</div>
 			<?php } ?>
 				
-				<div class="w100 frame">							
+				<div class="w100 frame">	
+				    <h3>Access Control</h3>
+				    <p>Manage Nodes, Cards, and Memberships</p>
+				    <ul>
+						<li>
+							<a href="<?php echo $base_url;?>node">Manage Nodes</a>			
+						</li>	
+						<li>
+							<a href="<?php echo $base_url;?>card/manage">Manage Cards</a>			
+						</li>	
+						<li>
+							<a href="<?php echo $base_url;?>membership/manage">Manage Memberships</a>			
+						</li>	
+					</ul>	
 					<h3>User Accounts</h3>
 					<p>Manage the account details of all site users.</p>
-					<p>
-						This example updates records from the required 'User Accounts' table, and from the custom 'Demo User Profile' table that in this demo is used to store a users name, phone number etc.<br/>
-						In addition, this demo also allows the privileges of users to be defined.   
-					</p>
 					<ul>
 						<li>
 							<a href="<?php echo $base_url;?>auth_admin/manage_user_accounts">Manage User Accounts</a>			
@@ -58,7 +67,6 @@
 
 					<h3>User Groups</h3>
 					<p>Manage the user groups that users can be assigned to.</p>
-					<p>User groups are intended to be used to categorise the primary access rights of a user, if required, more specific privileges can then be assigned to a user using the 'User Privileges' below. User groups are completely customised.</p>
 					<ul>
 						<li>
 							<a href="<?php echo $base_url;?>auth_admin/manage_user_groups">Manage User Groups</a>			
@@ -68,7 +76,6 @@
 
 					<h3>User Privileges</h3>
 					<p>Manage the specific user privileges that can be assigned to users.</p>
-					<p>User privileges are intended to verify whether a user has privileges to perfrom specific actions within the site. The specific action of each privilege is completely customised.</p>
 					<ul>
 						<li>
 							<a href="<?php echo $base_url;?>auth_admin/manage_privileges">Manage User Privileges</a>			
@@ -78,10 +85,6 @@
 
 					<h3>User Activity</h3>
 					<p>View lists of users that are currently active, inactive or who have a high number of failed logins attempts.</p>
-					<p>
-						When a user registers for an account, it is a good practice to have the user confirm their registration via email, as this helps prevent spam accounts being repeatedly setup.<br/>
-						Active (activated) account users can login, inactive accounts cannot. It is also possible in suspend an active account to prevent the user from logging in again.
-					</p>
 					<ul>
 						<li>
 							<a href="<?php echo $base_url;?>auth_admin/list_user_status/active">List all active users</a>
