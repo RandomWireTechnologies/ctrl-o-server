e96f4ec14ea8aff892971d3751cfa0c0890e12f5
Patrick Shields <patrick@randomwire.biz>
2015-01-22 01:06:09 -0500

    Updating sql updates for making changes work properly.

--- mysql_update_add_node_access.sql
+++ mysql_update_add_node_access.sql
@@ -9,12 +9,12 @@ create table if not exists access_group_users
 
 create table if not exists access_group_priveleges 
 (id int(6) unsigned primary key not null auto_increment, access_group_id int(6) unsigned not null, 
-node_id int(6) unsigned not null, schedule_id int(6) UNSIGNED default NULL, membership_type_id int(6) unsigned default NULL, 
+node_id int(6) not null, schedule_id int(6) UNSIGNED default NULL, membership_type_id int(6) default NULL, 
 INDEX grp_id_index (access_group_id), INDEX node_id_index (node_id));
 
 create table if not exists access_user_priveleges 
-(id int(6) unsigned primary key not null auto_increment, user_id int(6) unsigned not null, 
-node_id int(6) unsigned not null, schedule_id int(6) UNSIGNED default NULL, membership_type_id int(6) unsigned default NULL,
+(id int(6) unsigned primary key not null auto_increment, user_id int(6) not null, 
+node_id int(6) unsigned not null, schedule_id int(6) UNSIGNED default NULL, membership_type_id int(6) default NULL,
 INDEX uid_index (user_id), INDEX node_id_index (node_id));
 
 create table if not exists schedules (id int(6) unsigned primary key not null auto_increment, name VARCHAR(64) not null);
@@ -23,4 +23,15 @@ create table if not exists schedule_times (id int(6) unsigned primary key not nu
 schedule_id int(6) unsigned not null, day_of_week tinyint default NULL, start_date date default NULL, end_date date default NULL, 
 start_time TIME default NULL, end_time TIME default NULL, INDEX sch_id_index (schedule_id));
 
+create or replace view current_schedules as 
+select distinct schedule_id from schedule_times where 
+(day_of_week is NULL OR day_of_week=DAYOFWEEK(NOW())) AND 
+(start_date is NULL OR start_date <= DATE(NOW())) AND 
+(end_date is NULL OR end_date>= DATE(NOW())) AND 
+(start_time is null OR start_time <= TIME(NOW())) AND 
+(end_time is null OR end_time >= TIME(NOW()));
 
+create or replace view current_memberships as
+select * from memberships where start<=NOW() and end>=NOW();
+create table if not exists access_manual_unlock (id int(6) unsigned primary key not null auto_increment, name VARCHAR(64) not null, 
+node_id int(6) unsigned not null, schedule_id int(6) unsigned not null, enabled tinyint not null);
