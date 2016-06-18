5934bb6a32443991d1e3849c7f5b4c4df3064ec0
Patrick Shields <patrick@randomwire.biz>
2016-06-17 22:00:55 -0400

    Adding some code to start testing Paypal event hooks

---
+++ laravel/app/Http/Controllers/PaypalController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use PayPal\Rest\ApiContext;
+use PayPal\Auth\OAuthTokenCredential;
+use PayPal\Api\Amount;
+use PayPal\Api\Details;
+use PayPal\Api\Item;
+use PayPal\Api\ItemList;
+use PayPal\Api\Payer;
+use PayPal\Api\Payment;
+use PayPal\Api\RedirectUrls;
+use PayPal\Api\ExecutePayment;
+use PayPal\Api\PaymentExecution;
+use PayPal\Api\Transaction;
+
+use App\Http\Requests;
+
+class PaypalController extends Controller
+{
+    //
+    private $_api_context;
+ 
+    public function __construct()
+    {
+        ...
+        // setup PayPal api context
+        $paypal_conf = Config::get('paypal');
+        $this->_api_context = new ApiContext(new OAuthTokenCredential($paypal_conf['client_id'], $paypal_conf['secret']));
+        $this->_api_context->setConfig($paypal_conf['settings']);
+    }
+}
+
+
+<?php
+ 
+class PaypalController extends BaseController
+{
+    ...
+   
+    ...
+}
