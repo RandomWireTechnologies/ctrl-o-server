e0891e5d7114e6bc795e7b79d29bfd6557003266
Patrick Shields <patrick@randomwire.biz>
2016-07-18 01:01:22 -0400

    Updating interface to make listing existing subscriptions work

--- laravel/app/Http/Controllers/PaypalController.php
+++ laravel/app/Http/Controllers/PaypalController.php
@@ -6,6 +6,9 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use IPN;
+use PayPal\Api\Plan;
+use PayPal\Auth\OAuthTokenCredential;
+use PayPal\Rest\ApiContext;
 
 class PaypalController extends Controller
 {
@@ -17,7 +20,41 @@ class PaypalController extends Controller
 
     public function listSubscriptions()
     {
-        return view('paypal/listSubscriptions');
+        $subscriptions = array();
+        $subscription_raw = "Raw not set";
+
+        try {
+            $apiContext = new ApiContext(
+                new OAuthTokenCredential(
+                    env('PAYPAL_ID'),
+                    env('PAYPAL_SECRET')
+                )
+            );
+            $apiContext->setConfig(
+                array(
+                    'mode' => 'sandbox',
+                    'log.LogEnabled' => true,
+                    'log.FileName' => '../PayPal.log',
+                    'log.LogLevel' => 'DEBUG', // PLEASE USE `INFO` LEVEL FOR LOGGING IN LIVE ENVIRONMENTS
+                    'cache.enabled' => true,
+                    // 'http.CURLOPT_CONNECTTIMEOUT' => 30
+                    // 'http.headers.PayPal-Partner-Attribution-Id' => '123123123'
+                    //'log.AdapterFactory' => '\PayPal\Log\DefaultLogFactory' // Factory class implementing \PayPal\Log\PayPalLogFactory
+                )
+            );
+            // Get the list of all plans
+            // You can modify different params to change the return list.
+            // The explanation about each pagination information could be found here
+            // at https://developer.paypal.com/webapps/developer/docs/api/#list-plans
+            $params = array('page_size' => '2','status' => 'active');
+            $planList = Plan::all($params, $apiContext);
+            $subscriptions = $planList->getPlans();
+            $subscription_raw = $planList->toJSON();
+        } catch (Exception $ex) {
+            $subscription_raw = "Exception $ex during plan retrieval";
+        }
+        // Stub to fill in later
+        return view('paypal/listSubscriptions', compact('subscriptions','subscription_raw'));
     }
 }
 
--- laravel/resources/views/paypal/listSubscriptions.blade.php
+++ laravel/resources/views/paypal/listSubscriptions.blade.php
@@ -5,10 +5,21 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Paypal Subscriptions</div>
+                @if (count($subscriptions) > 0)
+                @foreach ($subscriptions as $subscription)
+                    <div class="panel-body">
+                    
+                        {{ $subscription->getName() }}
 
+                    </div>
+
+                @endforeach
+                @endif
+                <hr>
                 <div class="panel-body">
-                    Paypal Subscriptions
+                    Raw Data: <br/>
+                    {{ $subscription_raw }}
                 </div>
             </div>
         </div>
