8cf0af3f5a1d3dc9416a71b395227a558357aa42
Patrick Shields <patrick@randomwire.biz>
2016-09-04 10:50:54 -0400

    Removing all old code

--- application/views/includes/footer.php
+++
@@ -1,9 +0,0 @@
-	<div class="content_wrap footer_bg">
-		<div id="footer" class="content clearfix">
-			<div class="float_l" style="margin-top:35px;">
-				Copyright &copy; <?php echo date('Y');?>, Random Wire Technologies, LLC, All Rights Reserved.<br/>
-			</div>
-			<div class="float_r" style="margin-top:20px; text-align:center;">
-			</div>
-		</div>
-	</div>
